5c2cf94447d7146105b10cceddfc4d7adbf8d242
kfndhdb <nastenka.vasileva.03@bk.ru>
2023-03-21 18:56:18 +0500

    Внесла изменения

--- parent-project/bot/src/main/java/app/BotApplication.java
+++ parent-project/bot/src/main/java/app/BotApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @EnableConfigurationProperties(RecordBot.class)
 public class BotApplication {
     public static void main(String[] args) {
-        var ctx = SpringApplication.run(RecordBot.class, args);
+        var ctx = SpringApplication.run(BotApplication.class, args);
         RecordBot config = ctx.getBean(RecordBot.class);
         System.out.println(config);
     }
