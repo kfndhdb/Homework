2ea0a30c63ac3bc66e948659d6c7e9596b12c905
kfndhdb <nastenka.vasileva.03@bk.ru>
2023-03-25 11:57:48 +0500

    Сделала в модуле link-parser механизм для разбора URL.....

--- parent-project/link-parser/src/main/java/com/baeldung/App.java
+++ parent-project/link-parser/src/main/java/com/baeldung/App.java
@@ -1,13 +1,20 @@
 package com.baeldung;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите ссылку: ");
+        String URL = sc.nextLine();
+        BLParser linkParser = new BLParser(URL);
+        System.out.println(linkParser.getData());
     }
 }
--- parent-project/link-parser/src/main/java/com/baeldung/BLParser.java
+++ parent-project/link-parser/src/main/java/com/baeldung/BLParser.java
@@ -2,7 +2,7 @@ package com.baeldung;
 
 public record BLParser(String URL) {
     public String getData(){
-        if (!(URL.contains("https://github.com/") || URL.contains("https://stackoverflow.com/questions/"))){
+        if (!((URL.contains("https://github.com/") && countSymbols(URL,"/")==4) || URL.contains("https://stackoverflow.com/questions/"))){
             return "null";
         }
         else {
@@ -10,5 +10,10 @@ public record BLParser(String URL) {
             int link_type = URL.contains("https://github.com/") ? 1:0;
             return links[link_type].getData(URL);
         }
+    }
+
+    private int countSymbols(String a, String b) {
+        int i = a.length() - a.replace(b, "").length();
+        return i;
     }
 }
