b60659cdc31b8146ef78471ea0c3e2372dda32ee
kfndhdb <nastenka.vasileva.03@bk.ru>
2023-04-05 00:07:57 +0500

    добавила в BotApplication (new TgBot().startBot();)

--- parent-project/bot/src/main/java/app/BotApplication.java
+++ parent-project/bot/src/main/java/app/BotApplication.java
@@ -1,9 +1,10 @@
 package app;
-import bot.configuration.*;
 
+import bot.configuration.RecordBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import tgBotClasses.TgBot;
 
 
 @SpringBootApplication
@@ -12,6 +13,7 @@ public class BotApplication {
     public static void main(String[] args) {
         var ctx = SpringApplication.run(BotApplication.class, args);
         RecordBot config = ctx.getBean(RecordBot.class);
+        new TgBot().startBot();
         System.out.println(config);
     }
 }
