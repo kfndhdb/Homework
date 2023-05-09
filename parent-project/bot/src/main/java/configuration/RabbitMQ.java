f8bb21c65a7a5e056cffa337c423be8d6839ed8c
kfndhdb <nastenka.vasileva.03@bk.ru>
2023-05-10 00:48:53 +0500

    Исправила ошибку

--- parent-project/bot/src/main/java/configuration/RabbitMQ.java
+++ parent-project/bot/src/main/java/configuration/RabbitMQ.java
@@ -14,34 +14,39 @@ import java.util.HashMap;
 import java.util.Map;
 @Configuration
 public record RabbitMQ() {
-     public static final String QUEUE_MESSAGES = "messages-queue";
+    public static final String QUEUE_MESSAGES = "messages-queue";
     public static final String EXCHANGE_MESSAGES = "messages-exchange";
     public static final String QUEUE_MESSAGES_DLQ = "messages-dlq";
+
     @Bean
     Queue messagesQueue() {
         return QueueBuilder.durable(QUEUE_MESSAGES)
-                .withArgument("x-dead-letter-exchange", "")
-                .withArgument("x-dead-letter-routing-key", QUEUE_MESSAGES_DLQ)
-                .build();
+            .withArgument("x-dead-letter-exchange", "")
+            .withArgument("x-dead-letter-routing-key", QUEUE_MESSAGES_DLQ)
+            .build();
     }
 
     @Bean
     Queue deadLetterQueue() {
         return QueueBuilder.durable(QUEUE_MESSAGES_DLQ).build();
     }
+
     @Bean
-    public Binding bindingBean(Binding binding){
+    public Binding bindingBean(Binding binding) {
         return binding;
     }
+
     @Bean
-    public Queue queueBean(Queue queue){
+    public Queue queueBean(Queue queue) {
         return queue;
     }
+
     @Bean
-    public DirectExchange directExchangeBean(DirectExchange directExchange){
+    public DirectExchange directExchangeBean(DirectExchange directExchange) {
         return directExchange;
     }
-    public ClassMapper classMapper(){
+
+    public ClassMapper classMapper() {
         Map<String, Class<?>> mappings = new HashMap<>();
         mappings.put("dto_classes.DataClass", DataC.class);
 
@@ -50,9 +55,11 @@ public record RabbitMQ() {
         classMapper.setIdClassMapping(mappings);
         return classMapper;
     }
+
     @Bean
     public MessageConverter jsonMessageConverter(ClassMapper classMapper) {
-        Jackson2JsonMessageConverter jsonConverter=new Jackson2JsonMessageConverter();
+        Jackson2JsonMessageConverter jsonConverter = new Jackson2JsonMessageConverter();
         jsonConverter.setClassMapper(classMapper);
         return jsonConverter;
+    }
 }
